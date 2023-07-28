a1545cbbdd614bc423e49e25ab31fca6e47b91a1
Nidhi <Nidhi@cybergindia.local>
2023-07-28 17:04:50 +0530

    n

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ITGateway.Models;
+using ITGateway.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Http;
+namespace ITGateway.Controllers;
+
+public class HomeController : Controller
+{
+        public DataContext _context ;
+    
+    public HomeController(DataContext context )
+    {
+            _context = context;
+    }
+    // public ActionResult BeforeLayoutContent()
+    // {
+    //     // Your logic here
+    //     return PartialView("_BeforeLayoutContent");
+    // }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Admin(){
+        return View();
+    }
+     [HttpGet("/Home/Index")]
+public ActionResult<string> UserLogin(string userName, string Password)
+{
+    if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(Password))
+    {
+        // Render the login form
+        return View("Index");
+    }
+
+    var user = _context.UserInfo.SingleOrDefault(u => u.username == userName);
+    if (user != null && Password==user.password)
+    {
+        var token = GenerateJwtToken(user.username);
+       
+        // HttpContext.Session.SetString("JwtToken", token);
+        Response.Cookies.Append("JwtToken",token);
+        // localStorage.setItem("token", token);
+        return View("Admin");
+    }
+    return "Enter correct credentials";
+}
+
+// public static string GenerateSecurityKey()
+// {
+//     const string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+//     const int keyLength = 32; // Choose a suitable key length (in characters)
+    
+//     byte[] randomBytes = new byte[keyLength];
+
+//     using (var rng = new RNGCryptoServiceProvider())
+//     {
+//         rng.GetBytes(randomBytes);
+//     }
+
+//     StringBuilder result = new StringBuilder(keyLength);
+//     foreach (byte b in randomBytes)
+//     {
+//         result.Append(allowedChars[b % allowedChars.Length]);
+//     }
+
+//     return result.ToString();
+// }
+
+  private string GenerateJwtToken(string username)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("abjhbasjhbsjsjbjhabhshNidhi"));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, username),
+            // You can add more claims as needed, e.g., roles or additional user information
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: "http://localhost:5099",
+            audience: "http://localhost:5099",
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30), // Set the token expiration time as needed
+            signingCredentials: credentials
+        );
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+using System.Text;
+using ITGateway.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Session;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")); // Configure SQL Server connection
+        });
+        var key=Encoding.ASCII.GetBytes("abjhbasjhbsjsjbjhabhshNidhi");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                        .AddJwtBearer(options =>
+                        {
+                            options.TokenValidationParameters = new TokenValidationParameters
+                            {
+                                ValidateIssuer = false,
+                                ValidateAudience = false,
+                                ValidateIssuerSigningKey = true,
+                                ValidIssuer = "http://localhost:5099",
+                                ValidAudience = "http://localhost:5099",
+                                IssuerSigningKey = new SymmetricSecurityKey(key)
+                            };
+                        });
+//  builder.Services.AddDistributedSqlServerCache(options =>
+//         {
+//             options.ConnectionString = builder.Configuration.GetConnectionString("MyConnection");
+//             options.SchemaName = "dbo"; // Optional: Set the schema name for the database table
+//             options.TableName = "Sessions"; // Set the table name for storing sessions
+//         });
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+// app.UseSession();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSession();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
